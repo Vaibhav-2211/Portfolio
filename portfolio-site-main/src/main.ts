4d12d1fb2b655ebc4c97e80db8d24369433ad147
Vaibhav <mr.vishu1303@gmail.com>
2026-03-28 20:06:39 +0530

    done

--- portfolio-site-main/src/main.ts
+++ portfolio-site-main/src/main.ts
@@ -3,11 +3,7 @@ import { AppComponent } from './app/app.component';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-import { injectSpeedInsights } from '@vercel/speed-insights';
-import { inject } from '@vercel/analytics';
 
-inject();
-injectSpeedInsights();
 bootstrapApplication(AppComponent, {
   providers: [
     provideClientHydration(),
